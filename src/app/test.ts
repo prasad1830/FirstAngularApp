84d373196115a57736de49868635c40b358a41b3
santosh457 <58205516+santosh457@users.noreply.github.com>
2019-11-29 14:21:34 +0530

    New Method Added

--- src/app/test.ts
+++ src/app/test.ts
@@ -1,6 +1,7 @@
 export class prod {
     pInfo = [{ 'p01': 'test01' }, { 'p02': 'test02' }];
-    getProducts() {
-        return this.pInfo;
-    }
+
+  FetchProducts() {
+      console.log('hello World');
+  }
 }
